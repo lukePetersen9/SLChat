4533d47bb8a8d38fac2ec0726f89d68beb752ca4
Shubham Sinha <sinhashubham24@gmail.com>
2019-11-03 07:29:44 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(new MediaQuery(
+    data: new MediaQueryData(), child: new MaterialApp(home: new MyApp())));
+
+class MyApp extends StatelessWidget {
+  final databaseReference = Firestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FireStore Demo'),
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Create Record'),
+            onPressed: () {
+              createRecord();
+            },
+          ),
+          RaisedButton(
+            child: Text('View Record'),
+            onPressed: () {
+              getData();
+            },
+          ),
+          RaisedButton(
+            child: Text('Update Record'),
+            onPressed: () {
+              updateData();
+            },
+          ),
+          RaisedButton(
+            child: Text('Delete Record'),
+            onPressed: () {
+              deleteData();
+            },
+          ),
+        ],
+      )), //center
+    );
+  }
+
+  void createRecord() async {
+    await databaseReference.collection("books")
+        .document("1")
+        .setData({
+          'title': 'Mastering Flutter',
+          'description': 'Programming Guide for Dart'
+        });
+
+    DocumentReference ref = await databaseReference.collection("books")
+        .add({
+          'title': 'Flutter in Action',
+          'description': 'Complete Programming Guide to learn Flutter'
+        });
+    print(ref.documentID);
+  }
+
+  void getData() {
+    databaseReference
+        .collection("books")
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((f) => print('${f.data}}'));
+    });
+  }
+
+  void updateData() {
+    try {
+      databaseReference
+          .collection('books')
+          .document('1')
+          .updateData({'description': 'Head First Flutter'});
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void deleteData() {
+    try {
+      databaseReference
+          .collection('books')
+          .document('1')
+          .delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
